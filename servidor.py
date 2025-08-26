ad90f2f6249bafb8c20d198276bfc277b3e913c4
Lara <lara.abduni@gmail.com>
2025-08-26 19:20:17 -0300

    pagina em branco

--- servidor.py
+++ servidor.py
@@ -1,45 +1,13 @@
-from flask import Flask, render_template, request, redirect
-import views
+from flask import Flask, request
 
-app = Flask(__name__)  # sem template_folder aqui
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    notes = views.index()
-    return render_template('index.html', notes=notes)
+    print(request.method)
+    print(request.headers)
 
-@app.route('/submit', methods=['POST'])
-def submit_form():
-    titulo = request.form.get('titulo')
-    detalhes = request.form.get('detalhes')
-    views.submit(titulo, detalhes)
-    return redirect('/')
-
-@app.route('/delete/<int:note_id>', methods=['GET'])
-def delete(note_id):
-    views.delete(note_id)
-    return redirect('/')
-
-@app.route('/update/<int:note_id>', methods=['GET'])
-def edit(note_id):
-    nota = views.edit(note_id)
-    if nota:
-        # aqui você pode chamar só "components/edit.html"
-        return render_template('components/edit.html', nota=nota)
-    return redirect('/')
-
-@app.route('/update', methods=['POST'])
-def save_update():
-    note_id = request.form.get('id')
-    titulo = request.form.get('titulo')
-    detalhes = request.form.get('detalhes')
-    views.save_edit(note_id, titulo, detalhes)
-    return redirect('/')
-
-@app.route('/favorite/<int:note_id>', methods=['GET'])
-def favorite(note_id):
-    views.favorite(note_id)
-    return redirect('/')
+    return ""
 
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -3,23 +3,5 @@ from utils import load_data, load_template, armazena_coisas, deletar_nota, get_n
 
 def index():
     note_template = load_template('components/note.html')
-    notes_li = [note_template.format(id=dados['id'], title=dados['titulo'], details=dados['detalhes'],favorite_icon='★' if dados['favorita'] else '☆') for dados in load_data()]
-    return '\n'.join(notes_li)
+    return '\n'.join(note_template)
 
-def submit(titulo, detalhes):
-    armazena_coisas(titulo, detalhes)
-
-def delete(note_id):
-    deletar_nota(note_id)
-
-
-def edit(note_id):
-    return get_note_by_id(note_id)
-
-def favorite(note_id):
-    """Favorita ou desfavorita uma nota"""
-    toggle_favorite(note_id)
-    return redirect('/')
-
-def save_edit(note_id, titulo, detalhes):
-    update_note(note_id, titulo, detalhes)
