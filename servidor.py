0edf54dec3526fb97153298e19473c226eca1950
Rafael Vieira Bravo <rafaelvb@al.insper.edu.br>
2025-08-29 09:13:43 -0300

    Arquivos html agora tem uma base que só precisa ser extendida em outros arquivos

--- servidor.py
+++ servidor.py
@@ -9,6 +9,11 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/processo')
+def processo():
+    return render_template('processo.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
